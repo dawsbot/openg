e99c51bdb4fd1178c87b56dcac328ecef64d1377
Dawson Botsford <DawsonBotsford@gmail.com>
2016-07-04 11:25:28 -0700

    gulp functioning

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,21 +9,32 @@ const SRC = `${LERNA}/src/**`;
 const DEST = 'packages';
 // const DEST = `${LERNA}/dist`;
 const debug = require('gulp-debug');
+const glob = require('glob');
+const path = require('path');
 
-gulp.task('default', () => {
-    return gulp.src(SRC)
-        .pipe(debug('before': '1'))
-        .pipe(rename((path) => {
-          path.dirname.replace('src', 'dist');
-          return path;
-        }))
-				.pipe(debug())
-				.pipe(babel({
-					presets: [
-						'es2015',
-						'stage-2'
-					]
-				}))
-        .pipe(changed(DEST))
-        .pipe(gulp.dest(DEST));
+const packages = glob.sync(LERNA).map(function(packageDir) {
+  return path.basename(packageDir);
 });
+
+
+// build all packages
+gulp.task('build', packages.map(function(name){
+  packages.forEach((name) => {
+    gulp.task(`${name}-build`, () => {
+      return gulp.src(`packages/${name}/src/**`, {base: process.cwd()})
+      .pipe(rename(path => {
+        path.dirname = path.dirname.replace('src', 'dist')
+        return path;
+      }))
+      .pipe(debug())
+      .pipe(babel({
+        presets: [
+        'es2015',
+        'stage-2'
+        ]
+      }))
+      .pipe(gulp.dest(''));
+    });
+  });
+  return `${name}-build`;
+}));
